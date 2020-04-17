e38d8d7bf69db066e44be896145429254061e216
Michael Stevens <stevensmike36@yahoo.com>
2020-04-17 14:26:57 -0500

    answers done,failing tests  recursive, plan laid out on robot

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,25 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+#I will  get word but if it has a length of one I cannot compre it to my two 
+# my string "th" I need to start at the begging and take in the first and second 
+# elments and compare them to my string if it matches then count as one and move o
+
 def count_th(word):
+    #set base case
+    if len(word) <= 1:
+        return  1  
     
-    # TBC
+    if word.upper().count("th",0):
+        
+       
+        return count_th(word[0:])
+        
     
-    pass
+       
+    
+    
+        
+    
+    
+   
