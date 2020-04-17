48f7b20f93dc059a7a1d73bec4e42bbc517b87b6
Michael Stevens <stevensmike36@yahoo.com>
2020-04-17 17:52:28 -0500

    got count_th passing all 5

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,17 +8,24 @@ Your function must utilize recursion. It cannot contain any loops.
 # elments and compare them to my string if it matches then count as one and move o
 
 def count_th(word):
-    #set base case
-    if len(word) <= 1:
-        return  1  
-    
-    if word.upper().count("th",0):
+#    base case:
+    s = "th"
+    w = len(word)
+    l = len(s)
+    if w == 0 or w < l:
+        return 0
+#if length is zero)
+#return 0;
+# if it matches, increment occurrences
+    if word[0 : l] == s:
+        return count_th(word[l - 1:]) +1
+    return count_th(word[l - 1:])
+
+
+
         
-       
-        return count_th(word[0:])
+ 
         
-    
-       
     
     
         
